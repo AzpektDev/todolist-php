234affafe389a8db96bb2c70f7503d991aa78a15
Azpekt <65199167+AzpektDev@users.noreply.github.com>
2025-05-23 18:27:59 +0200

    finished the logic ig

--- dash.php
+++ dash.php
@@ -25,8 +25,13 @@
       </form>
       <?php 
          if ($_POST) {
-            if ($_POST['title'] && $_POST['description']) {
+            if (isset($_POST['title']) && isset($_POST['description'])) {
                $query = "INSERT INTO `todos` (user_id, title, description, status) VALUES (" . $user_id . ", '" . $_POST['title'] . "', '" . $_POST['description'] . "', 0);";
+               $res = mysqli_query($conn, $query);
+
+               header("Refresh: 0");
+            } else if (isset($_POST['id'])) {
+               $query = "UPDATE todos SET status = CASE WHEN status = 0 THEN 1 ELSE 0 END WHERE id = " . $_POST['id'] . ";";
                $res = mysqli_query($conn, $query);
 
                header("Refresh: 0");
@@ -42,21 +47,27 @@
          <th>id</th>
          <th>title</th>
          <th>description</th>
-         <th>status</th> 
-         <!-- ^^^^^^ change to completed??? -->
+         <th>completed?</th> 
          <th>update status</th>
       </tr>
       <?php
          $tasks = mysqli_query($conn, "SELECT * FROM todos WHERE user_id = '" . $user_id . "';");
 
          while ($task=mysqli_fetch_assoc($tasks)) {
-         echo "<tr>
-                  <td>" . $task['id'] . "</td>
-                  <td>" . $task['title'] . "</td>
-                  <td>" . $task['description'] . "</td>
-                  <td>" . $task['status'] . "</td>
-                  <td><button>update</button></td>
-               </tr>";
+            if ($task['status'] == 0) {
+               $status = "no";
+            } else {
+               $status = "yes";
+            }
+
+
+            echo "<tr>
+                     <td>" . $task['id'] . "</td>
+                     <td>" . $task['title'] . "</td>
+                     <td>" . $task['description'] . "</td>
+                     <td>" . $status . "</td>
+                     <td><form method=\"POST\"><input type=\"hidden\" name=\"id\" value=" . $task['id'] . "><button type=\"submit\">update</button></form></td>
+                  </tr>";
          }
 
          // $query = "SELECT * FROM todos WHERE "
