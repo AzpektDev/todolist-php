4d84dc594f73d43e095481027a335cb9ff5f36ca
Azpekt <65199167+AzpektDev@users.noreply.github.com>
2025-05-23 18:00:35 +0200

    task listing

--- dash.php
+++ dash.php
@@ -3,10 +3,43 @@
 ?>
 <html>
 <head>
-   <title>Welcome </title>
+   <title>Dashboard </title>
 </head>
 <body>
+   <?php
+      $conn = mysqli_connect("localhost", "root", "", "todo_list");
+   ?>
+
    <h1>Welcome <?php echo $login_session; ?></h1> 
+
+   <table>
+      <tr>
+         <th>id</th>
+         <th>title</th>
+         <th>description</th>
+         <th>status</th>
+         <th>update status</th>
+      </tr>
+      <?php
+         $res = mysqli_query($conn, "SELECT id FROM users WHERE username = '" . $login_session . "';");
+         $user_id = mysqli_fetch_assoc($res)['id'];
+
+         $tasks = mysqli_query($conn, "SELECT * FROM todos WHERE user_id = '" . $user_id . "';");
+
+         while ($task=mysqli_fetch_assoc($tasks)) {
+         echo "<tr>
+                  <td>" . $task['id'] . "</td>
+                  <td>" . $task['title'] . "</td>
+                  <td>" . $task['description'] . "</td>
+                  <td>" . $task['status'] . "</td>
+                  <td><button>update</button></td>
+               </tr>";
+         }
+
+         // $query = "SELECT * FROM todos WHERE "
+      ?>
+   </table>
+
    <a href = "logout.php"><button>Sign Out</button></a>
 </body>
 </html>
